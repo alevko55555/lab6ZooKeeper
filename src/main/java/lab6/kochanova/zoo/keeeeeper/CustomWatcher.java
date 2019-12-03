7c427754850ebacbdb98e02e441461f5d3ba876c
alevko55555 <alevko55555@gmail.com>
2019-12-03 17:54:22 +0300

    Scripted auto-commit on change (2019-12-03 17:54:22) by gitwatch.sh

--- src/main/java/lab6/kochanova/zoo/keeeeeper/CustomWatcher.java
+++ src/main/java/lab6/kochanova/zoo/keeeeeper/CustomWatcher.java
@@ -1,5 +1,6 @@
 package lab6.kochanova.zoo.keeeeeper;
 
+import akka.actor.ActorRef;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
@@ -7,22 +8,22 @@ import org.apache.zookeeper.ZooKeeper;
 public class CustomWatcher implements Watcher {
     private boolean init;
     private ZooKeeper zoo;
+    private ActorRef actor;
 
     public CustomWatcher(boolean init, ZooKeeper zoo){
         this.init = init;
         this.zoo = zoo;
     }
+    public CustomWatcher(){
+
+    }
 
     @Override
     public void process(WatchedEvent watchedEvent) {
-        if(init){
-
-        }else{
-
-        }
+        // process
     }
 
-    void refreshWatcher(){
-        zoo.getChildren()
+    void sendDataToActor(){
+       // sending data
     }
 }
--- src/main/java/lab6/kochanova/zoo/keeeeeper/Main.java
+++ src/main/java/lab6/kochanova/zoo/keeeeeper/Main.java
@@ -1,18 +1,31 @@
 package lab6.kochanova.zoo.keeeeeper;
 
+import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
 
 import java.io.IOException;
+
+/*
+ZooKeeper лежит в корневой папке пользователя
+запускается в папке bin/zkServer.sh [stop/start/status] конфиг файл лежит в папке confs (порт 127.0.0.1:2181)
+new ZooKeper(127.0.0.1:2181) -> подключается к уже запущенному на компьютере локально ZooKeeper
+watcher срабатывает строго один раз, поэтому его нужно обновлять
+*/
 
 public class Main {
     private ZooKeeper zooKeeper;
 
     public static void main(String[] args){
+        // creating server
+
 
     }
 
-    private void createZooKeeper(String host) throws IOException, KeeperException, InterruptedException {
-        zooKeeper = new ZooKeeper(host, 2000, new CustomWatcher());
+    private void createZooKeeper(String ZooKeeperHost, String serverHost) throws IOException, KeeperException, InterruptedException {
+        zooKeeper = new ZooKeeper(ZooKeeperHost, 2000, new CustomWatcher());
+        zooKeeper.create("/servers/" + serverHost, serverHost.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+
     }
 }
