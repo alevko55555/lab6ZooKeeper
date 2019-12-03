9e188d2b3305aab5f2b0253fde05207f55643ea1
alevko55555 <alevko55555@gmail.com>
2019-12-03 17:43:44 +0300

    Scripted auto-commit on change (2019-12-03 17:43:44) by gitwatch.sh

--- src/main/java/lab6/kochanova/zoo/keeeeeper/CustomWatcher.java
+++ src/main/java/lab6/kochanova/zoo/keeeeeper/CustomWatcher.java
@@ -1,12 +1,22 @@
-package lab6.kochanova.zooKeeper;
+package lab6.kochanova.zoo.keeeeeper;
 
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
 
 public class CustomWatcher implements Watcher {
+    boolean init;
+
+    public CustomWatcher(boolean init){
+        this.init = init;
+    }
 
     @Override
     public void process(WatchedEvent watchedEvent) {
 
+    }
+
+    void refreshWatcher(){
+        
     }
 }
--- src/main/java/lab6/kochanova/zoo/keeeeeper/Main.java
+++ src/main/java/lab6/kochanova/zoo/keeeeeper/Main.java
@@ -1,11 +1,18 @@
-package lab6.kochanova.zooKeeper;
+package lab6.kochanova.zoo.keeeeeper;
 
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
 
 public class Main {
     private ZooKeeper zooKeeper;
 
     public static void main(String[] args){
 
+    }
+
+    private void createZooKeeper(String host) throws IOException, KeeperException, InterruptedException {
+        zooKeeper = new ZooKeeper(host, 2000, new CustomWatcher());
     }
 }
