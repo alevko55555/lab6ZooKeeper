b2611d739f14e4555d63af7fba62a0ab11576865
alevko55555 <alevko55555@gmail.com>
2019-12-07 00:34:05 +0300

    Scripted auto-commit on change (2019-12-07 00:34:05) by gitwatch.sh

--- src/main/java/lab6/kochanova/zoo/keeeeeper/ActorStorageConfig.java
+++ src/main/java/lab6/kochanova/zoo/keeeeeper/ActorStorageConfig.java
@@ -2,9 +2,19 @@ package lab6.kochanova.zoo.keeeeeper;
 
 import akka.actor.AbstractActor;
 
+import java.util.List;
+
 public class ActorStorageConfig extends AbstractActor {
+    private final List<String> storage;
+
+    public ActorStorageConfig(List<String> storage) {
+        this.storage = storage;
+    }
+
     @Override
     public Receive createReceive() {
-        return null;
+        return receiveBuilder()
+                .match()
+                .build();
     }
 }
