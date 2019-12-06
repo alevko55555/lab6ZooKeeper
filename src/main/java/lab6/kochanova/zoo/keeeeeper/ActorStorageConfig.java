8bbd593e0bbf6917ebeff304b022c8b9f4fa6aa7
alevko55555 <alevko55555@gmail.com>
2019-12-07 00:44:55 +0300

    Scripted auto-commit on change (2019-12-07 00:44:55) by gitwatch.sh

--- src/main/java/lab6/kochanova/zoo/keeeeeper/ActorStorageConfig.java
+++ src/main/java/lab6/kochanova/zoo/keeeeeper/ActorStorageConfig.java
@@ -1,15 +1,23 @@
 package lab6.kochanova.zoo.keeeeeper;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import message.DeleteServer;
+import message.GetRandomServer;
 import message.PutServer;
+import message.ServerList;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class ActorStorageConfig extends AbstractActor {
     private final List<String> storage;
+    private final Random random;
 
-    public ActorStorageConfig(List<String> storage) {
-        this.storage = storage;
+    public ActorStorageConfig() {
+        this.storage = new ArrayList<>();
+        this.random = new Random();
     }
 
     @Override
@@ -18,6 +26,15 @@ public class ActorStorageConfig extends AbstractActor {
                 .match(PutServer.class, msg -> {
                     storage.clear();
                     storage.addAll(msg.getServers());
+                })
+                .match(GetRandomServer.class, msg -> {
+                    getSender().tell(
+                            new ServerList(storage.get(random.nextInt(storage.size()))),
+                            ActorRef.noSender()
+                    );
+                })
+                .match(DeleteServer.class, msg -> {
+                    storage.remove(msg.getServer());
                 })
                 .build();
     }
