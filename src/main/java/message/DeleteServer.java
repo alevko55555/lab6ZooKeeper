05e7cdd52a3f1b6823b6da0f6ae0bc37b96ab7a4
alevko55555 <alevko55555@gmail.com>
2019-12-07 00:36:52 +0300

    Scripted auto-commit on change (2019-12-07 00:36:52) by gitwatch.sh

--- src/main/java/message/DeleteServer.java
+++ src/main/java/message/DeleteServer.java
@@ -1,4 +1,13 @@
 package message;
 
 public class DeleteServer {
+    private final String server;
+
+    public DeleteServer(String server) {
+        this.server = server;
+    }
+
+    public String getServer() {
+        return server;
+    }
 }
