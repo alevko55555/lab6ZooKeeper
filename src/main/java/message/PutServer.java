db117d38bad315f4c64a29fa7013247359b836e9
alevko55555 <alevko55555@gmail.com>
2019-12-07 00:35:18 +0300

    Scripted auto-commit on change (2019-12-07 00:35:18) by gitwatch.sh

--- src/main/java/message/PutServer.java
+++ src/main/java/message/PutServer.java
@@ -1,4 +1,11 @@
 package message;
 
+import java.util.List;
+
 public class PutServer {
+    private final List<String> servers;
+
+    public PutServer(List<String> servers) {
+        this.servers = servers;
+    }
 }
