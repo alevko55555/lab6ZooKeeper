c62a9368770fa46b2adf5f344e0f04d1859a0b6c
alevko55555 <alevko55555@gmail.com>
2019-12-13 15:58:13 +0300

    Scripted auto-commit on change (2019-12-13 15:58:13) by gitwatch.sh

--- src/main/java/lab6/kochanova/zoo/keeeeeper/Anonymizator.java
+++ src/main/java/lab6/kochanova/zoo/keeeeeper/Anonymizator.java
@@ -3,6 +3,8 @@ package lab6.kochanova.zoo.keeeeeper;
 import akka.actor.ActorRef;
 import akka.http.javadsl.server.Route;
 import akka.pattern.Patterns;
+import akka.stream.ConnectionException;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooKeeper;
 import org.asynchttpclient.AsyncHttpClient;
 
@@ -10,8 +12,10 @@ import message.DeleteServer;
 import message.GetRandomServer;
 import message.PutServer;
 import message.ServerList;
+import org.asynchttpclient.Request;
 import org.asynchttpclient.Response;
 
+import java.net.ConnectException;
 import java.time.Duration;
 import java.util.concurrent.CompletionStage;
 
@@ -43,7 +47,36 @@ public class Anonymizator {
             asyncHttpClient.executeRequest(asyncHttpClient.prepareGet(url).build()).toCompletableFuture();
         } else {
             Patterns.ask(storage, new GetRandomServer(), Duration.ofSeconds(5))
-                    .thenApply(o -> )
+                    .thenApply(o -> ((ServerList)o).getServer())
+                    .thenCompose(znode -> asyncHttpClient.executeRequest(
+                            createServerRequest(getServerUrl(znode), url, count-1)
+                    ).toCompletableFuture()
+                                    .handle((response, throwable) ->
+                                            handleBadRedirection(response, throwable, znode))
+                    );
+        }
+    }
+
+    public Response handleBadRedirection(Response response, Throwable throwable, String znode) {
+        if(throwable instanceof ConnectionException) {
+            storage.tell(new DeleteServer(znode), ActorRef.noSender());
+        }
+        return response;
+    }
+
+    public Request createServerRequest(String serverUrl, String url, int count) {
+        return asyncHttpClient.prepareGet(serverUrl)
+                .addQueryParam("url", url)
+                .addQueryParam("count", Integer.toString(count))
+                .build();
+    }
+
+    public String getServerUrl(String znode) {
+        try {
+            return new String(zoo.getData(znode, false, null));
+        }
+        catch(KeeperException | InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 }
