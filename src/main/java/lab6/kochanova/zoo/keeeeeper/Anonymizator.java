ce200ec20bc2ac9da9e39a935e6d12b55388476a
alevko55555 <alevko55555@gmail.com>
2019-12-13 15:02:35 +0300

    Scripted auto-commit on change (2019-12-13 15:02:35) by gitwatch.sh

--- src/main/java/lab6/kochanova/zoo/keeeeeper/Anonymizator.java
+++ src/main/java/lab6/kochanova/zoo/keeeeeper/Anonymizator.java
@@ -1,4 +1,24 @@
 package lab6.kochanova.zoo.keeeeeper;
 
+import akka.actor.ActorRef;
+import akka.http.javadsl.server.Route;
+import org.apache.zookeeper.ZooKeeper;
+import org.asynchttpclient.AsyncHttpClient;
+
+import static akka.http.javadsl.server.Directives.route;
+
 public class Anonymizator {
+    private final AsyncHttpClient asyncHttpClient;
+    private final ActorRef storage;
+    private final ZooKeeper zoo;
+
+    public Anonymizator (ActorRef storage, AsyncHttpClient asyncHttpClient, ZooKeeper zoo) {
+        this.storage = storage;
+        this.asyncHttpClient = asyncHttpClient;
+        this.zoo = zoo;
+    }
+
+    public Route createRoute() {
+        return route(get(() -> ));
+    }
 }
